a5876fdcd5f1846ea1f612399602eb4f6cf97acb
Sahil nadaf <119119664+sahilnadaf2003@users.noreply.github.com>
2022-12-29 23:22:27 +0530

    Add files via upload

---
+++ class36--iteratorvsiterable.py
@@ -0,0 +1,18 @@
+numbers = [20,30,40]  
+#  tuples, strings are iterables
+cubes = map(lambda a:a**3,numbers)
+# iterator
+print(cubes)
+for i in cubes:
+    print(i)
+for i in numbers:
+    print(numbers)    
+
+# iter function is used to call  
+iter(numbers)
+next(iter(numbers))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pri
---
+++ class39-advancefunction.py
@@ -0,0 +1,7 @@
+def avg(x,y):
+    average = []
+    for pair in zip(x,y):
+        average.append(sum(pair)/len(pair))
+    return average
+ 
+print(avg([25,26,80],[70,20,45])) 
